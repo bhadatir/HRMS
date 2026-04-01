f5fb0e6a1de2c96baa71c55f41478e9fec0de4bd
TirthBhadani <tirth.bhadani@roimaint.com>
2026-04-01 15:51:01 +0530

    Update Loading spinner Ui.

--- HRMS-Frontend/src/components/ui/spinner.tsx
+++ HRMS-Frontend/src/components/ui/spinner.tsx
@@ -1,20 +1,26 @@
 import { cn } from "@/lib/utils";
+import roimaSpinner from "@/assets/roima_loader.png";
 
 type SpinnerProps = {
   className?: string;
   sizeClassName?: string;
 };
 
-export function Spinner({ className, sizeClassName = "h-12 w-12" }: SpinnerProps) {
+export function Spinner({ className, sizeClassName = "h-32 w-32" }: SpinnerProps) {
   return (
     <div className="fixed inset-0 bg-black/50 z-50 flex items-center justify-center">
+      <div className={cn("relative flex items-center justify-center", sizeClassName)}>
+        <span className="px-4 py-2">
+          <img src={roimaSpinner} alt="Roima Spinner" className="h-24 w-auto" />
+        </span>
+
         <div
-        className={cn(
-            "animate-spin rounded-full border-b-2 border-white",
-            sizeClassName,
+          className={cn(
+            "absolute inset-0 rounded-full border-4 border-white/25 border-t-white animate-spin",
             className
-        )}
+          )}
         />
+      </div>
     </div>
   );
 }
